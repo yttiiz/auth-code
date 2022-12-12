c056b020443ddb84999d6f4acfd64510da6ff425
yttiiz <dominique.talis@gmail.com>
2022-12-12 12:26:50 +0100

    alphacap test - step 2 (progress on css)

--- login.js
+++ login.js
@@ -1,3 +1,5 @@
 const form = document.querySelector('form')
+const phoneNumber = document.querySelector('strong')
 
-console.log(form)
+//Adding none breakable spaces between each numbers.
+phoneNumber.innerHTML = phoneNumber.textContent.split(' ').join('&nbsp;')
