6d6cba670df21be76c5b6081de51660741ac309d
yttiiz <dominique.talis@gmail.com>
2022-12-12 14:57:48 +0100

    alphacap test - step 3 (css done | progress on js)

--- login.js
+++ login.js
@@ -1,5 +1,37 @@
-const form = document.querySelector('form')
-const phoneNumber = document.querySelector('strong')
+// =====================| Elements |=====================//
+const inputs = [].slice.call(document.querySelector('.code-auth-content form').querySelectorAll('input'))
+const phoneNumber = document.querySelector('strong'), errorMsgContainer = document.querySelector('.alert-error span')
+const errorMsgTitle = 'Code incorrect', errorMsgContent = 'merci de vérifier votre saisie'
 
+// =====================| Events |=====================//
 //Adding none breakable spaces between each numbers.
 phoneNumber.innerHTML = phoneNumber.textContent.split(' ').join('&nbsp;')
+
+handleErrorMessage(errorMsgContainer)
+document.addEventListener('DOMContentLoaded', handleDocument)
+
+// =====================| Functions |=====================//
+function createElt(type) {
+    return document.createElement(type)
+}
+
+function handleErrorMessage(el) {
+    const strong = createElt('strong'), span = createElt('span')
+    let elementHeight
+    strong.textContent = errorMsgTitle
+    span.textContent = errorMsgContent
+    
+    el.appendChild(strong)
+    el.appendChild(span)
+    
+    elementHeight = el.clientHeight
+    el.style.top = `${elementHeight}px`
+}
+
+function handleDocument() {
+    inputs.forEach((input, i) => {
+        if (i === 0) {
+            console.log(input)
+        }
+    })
+}
